4dad91add6241f79990e376e39a75ab927257e61
VPSHOZOO <lordhozoo891@gmail.com>
2025-04-24 10:28:43 +0700

    Create HOZOO.sh

---
+++ HOZOO.sh
@@ -0,0 +1,15 @@
+
+sudo apt-get install git -y
+
+sudo apt-get install nodejs -y
+
+sudo apt-get install npm -y
+
+npm install axios
+
+npm install node-telegram-bot-api
+
+npm install express
+
+node 
+
